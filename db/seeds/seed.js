84d849ab27e359ef105055d3a972aafdf3cd9a43
Harps1000 <Harpreet101gill@gmail.com>
2019-10-28 11:13:33 +0000

    database seeded

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,10 +1,33 @@
-// const {  } = require('../data');
+const { articles, comments, topics, users } = require("../data/index");
+
+const { createDate, createRef, formatData, formatData2 } = require("../../utils/utils");
 
 exports.seed = (knex, Promise) => {
   return knex.migrate
     .rollback()
     .then(() => knex.migrate.latest())
     .then(() => {
-      // insert data
+      return knex("topics").insert(topics, ["*"]);
+    })
+    .then(() => {
+      return knex("users").insert(users, ["*"]);
+    })
+    .then(() => {
+      const data = createDate(articles);
+      return knex("articles").insert(data, ["*"]);
+    })
+    .then(articles => {
+      const refobj = createRef(articles, "title", "article_id");
+     
+      const finaldata = formatData(
+        comments,
+        refobj,
+        "belongs_to",
+        "article_id"
+      );
+      const dataToInsert = createDate(finaldata);
+     const sData = formatData2(dataToInsert,"created_by", "author");
+
+     return knex("comments").insert(sData, ["*"]);
     });
 };
