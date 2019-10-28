ade9d8168cf70129d0141ba45f55eb69bc1097a2
Harps1000 <Harpreet101gill@gmail.com>
2019-10-28 11:06:20 +0000

    articles migration table

---
+++ db/migrations/20191028110503_articles.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("articles", art => {
+        art.increments("article_id").primary();
+        art.string("title");
+        art.text("body");
+        art.integer("votes").defaultTo(0);
+        art.string("topic").references("topics.slug");
+        art.string("author").references("users.username");
+        art.timestamp("created_at").defaultTo(knex.fn.now());
+      });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable("articles");
+};
