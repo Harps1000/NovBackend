3f55ddf6a32d5ab5261510506b86d4f1ec334d8a
Harps1000 <Harpreet101gill@gmail.com>
2019-10-28 14:25:14 +0000

    reviewing

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const apiRouter = require('./routes/api');
-const { routeNotFound, handle500 } = require('./errors');
+const express = require("express");
+const apiRouter = require("./routes/api");
+const { routeNotFound, handle500, otherErrors, psqlErrors, otherPsqlErrors } = require("./errors");
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
-app.all('/*', routeNotFound);
+app.all("/*", routeNotFound);
+
+app.use(otherErrors);
+
+app.use(psqlErrors);
+
+app.use(otherPsqlErrors);
 
 app.use(handle500);
 
